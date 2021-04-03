47f8be6df4b305713ef4cf4aa420b3673aef4986
desxz <muratgun545@gmail.com>
2021-04-03 13:05:37 +0300

    Button Model and Assets List

--- cryptocurrency_mobile/lib/view/home_screen/model/button_model.dart
+++ cryptocurrency_mobile/lib/view/home_screen/model/button_model.dart
@@ -41,3 +41,62 @@ class Button {
 
   factory Button.fromJson(String source) => Button.fromMap(json.decode(source));
 }
+
+List<Map<String, dynamic>> buttons = [
+  {
+    "coinName": "BITCOIN",
+    "height": '',
+    "width": '',
+    "color": Color(0xFFF2A900),
+    "icon": "assets/BTC.svg",
+  },
+  {
+    "coinName": "Ethereum",
+    "height": "",
+    "width": "",
+    "color": Color(0xFF191921),
+    "icon": "assets/Etherium.svg",
+  },
+  {
+    "coinName": "RIPPLE",
+    "height": "",
+    "width": "",
+    "color": Color(0xFF00AAE4),
+    "icon": "assets/Ripple.svg",
+  },
+  {
+    "coinName": "DOGECOIN",
+    "height": "",
+    "width": "",
+    "color": Color(0xFFCB9800),
+    "icon": "assets/Dogecoin.svg",
+  },
+  {
+    "coinName": "LITECOIN",
+    "height": "",
+    "width": "",
+    "color": Color(0xFF00AEFF),
+    "icon": "assets/litecoin.svg",
+  },
+  {
+    "coinName": "TETHER",
+    "height": "",
+    "width": "",
+    "color": Color(0xFF26A17B),
+    "icon": "assets/Tether.svg",
+  },
+  {
+    "coinName": "BINANCE C.",
+    "height": "",
+    "width": "",
+    "color": Color(0xFFF3BA2F),
+    "icon": "assets/Binance.svg",
+  },
+  {
+    "coinName": "BITTORRENT",
+    "height": "",
+    "width": "",
+    "color": Color(0xFF000000),
+    "icon": "assets/Bittorrent.svg",
+  },
+];
--- cryptocurrency_mobile/lib/view/home_screen/view/home_screen.dart
+++ cryptocurrency_mobile/lib/view/home_screen/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,60 +13,83 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Color(0xff27303d),
         elevation: 0,
-        leading: Icon(Icons.ac_unit),
       ),
       backgroundColor: Color(0xff27303d),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(Icons.ac_unit),
-              Text(
-                'CRYPTO\nCURRENCY',
-                textAlign: TextAlign.end,
-                style: Theme.of(context).textTheme.headline4.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-            ],
-          ),
-          Form(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 33, horizontal: 18),
-              child: Container(
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Color(0xff414954),
-                  border: Border.all(
-                      color: Colors.transparent, // set border color
-                      width: 3.0), // set border width
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(10.0)), // set rounded corner radius
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SvgPicture.asset(
+                  'assets/Logo.svg',
+                  color: Colors.white,
                 ),
-                child: Padding(
-                  padding: EdgeInsets.only(top: 0, left: 10),
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      hintText: 'Enter something...',
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: InputBorder.none,
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          Icons.send,
-                          color: Colors.white,
+                Text(
+                  'CRYPTO\nCURRENCY',
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ],
+            ),
+            Form(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 33, horizontal: 18),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0xff414954),
+                    border: Border.all(
+                        color: Colors.transparent, // set border color
+                        width: 3.0), // set border width
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(10.0)), // set rounded corner radius
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 0, left: 10),
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: 'Enter something...',
+                        hintStyle: TextStyle(color: Colors.white),
+                        border: InputBorder.none,
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
                         ),
-                        onPressed: () {},
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemCount: 7,
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xff414954),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
