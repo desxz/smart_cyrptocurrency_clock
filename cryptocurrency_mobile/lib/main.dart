c4a2200f53f7fba8b3e7793f19ced29070a7d597
desxz <muratgun545@gmail.com>
2021-04-03 08:17:19 +0300

    Mobile App Design

--- cryptocurrency_mobile/lib/main.dart
+++ cryptocurrency_mobile/lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'view/home_screen/view/home_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
