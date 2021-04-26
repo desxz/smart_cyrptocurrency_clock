31c186dd7912d34cb8197e8b8fb657e73d0b9537
desxz <muratgun545@gmail.com>
2021-04-26 21:22:33 +0300

    Flutter Null Safety Re-Start Project

---
+++ crypto_currency_app/android/app/src/main/kotlin/com/example/crypto_currency_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crypto_currency_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- cryptocurrency_mobile/android/app/src/main/kotlin/com/example/cryptocurrency_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cryptocurrency_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
