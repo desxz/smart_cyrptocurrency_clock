2c90909c77d6b24b30df9ac3ef50230f8ac4acac
Onur Duman <onurdumn99@gmail.com>
2021-05-16 22:10:31 +0300

    Firebase auth added

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -2,18 +2,23 @@
 #include "ESP8266WiFi.h"
 #include "ESP8266HTTPClient.h"
 #include "ArduinoJson.h"
+#include "FirebaseESP8266.h"
+
 
 // Wifi Details
 const char* ssid = "SUPERONLINE-WiFi_3177"; //Enter SSID
 const char* password = "PYVT4NU9UREA"; //Enter Password
 const int httpsPort = 443;
 
+// Firebase Details
+#define FIREBASE_HOST "crypto-currency-75f19-default-rtdb.europe-west1.firebasedatabase.app"
+#define FIRABASE_AUTH "0HlBwDs2lpRF3xAXDEyNqxUG31k4LeDp7Zyn5K6N"
+
 // API
-const String url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
 
 HTTPClient http;
 WiFiClientSecure client;
-
+FirebaseData fbdo;
 
 void setup(void)
 { 
@@ -30,6 +35,14 @@ void setup(void)
   Serial.println(ssid);
   Serial.print("ESP8266 Modül'ünün IP Adresi:");
   Serial.print(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIRABASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  Serial.print("Setup olayını bitirdim.");
+
+
+  
 }
 
 void loop() 
@@ -37,14 +50,43 @@ void loop()
   Serial.println("");
   Serial.println("");
   Serial.println("[TEST] Loop'a giriş yapıldı.");
-  ApiRequest();
+  firebaseauth();
   
   
   delay(20000); //Coindesk's API updates once twenty seconds
 }
 
+void firebaseauth(){
+  Serial.print("authdayım");
 
-void ApiRequest(){
+
+  String id = "-M_qY8ZibGmEyjVr2fOf"; 
+
+  
+  Firebase.get(fbdo,"/Users/Kemal/"+id+"/alarms");  //nu satırda 2.dönüşte çöküyor.
+  FirebaseJson json = fbdo.jsonObject();
+  int len = json.iteratorBegin();
+
+  if(len==0){
+    Serial.print("/Users/"+id+"/alarms veri yoluna erişim yok yada bu yolda hiç alarm yok." );
+  } 
+  else {
+    String key, value = "";
+    int type = 0;
+    for (int i = 0; i < len; i++){
+      json.iteratorGet(i, type, key, value);
+      ApiRequest(key);
+    }
+  }
+  json.iteratorEnd();
+  
+}
+
+
+void ApiRequest(String key){
+
+  String url = "https://api.coingecko.com/api/v3/simple/price?ids="+key+"&vs_currencies=usd";
+
 
   Serial.println("[TEST] APIRequest Çalıştı!");
 
@@ -63,10 +105,10 @@ void ApiRequest(){
     String payload = http.getString();
     DynamicJsonDocument jsonBuffer(1100);
     deserializeJson(jsonBuffer, payload);
-    JsonObject coin = jsonBuffer["bitcoin"];
+    JsonObject coin = jsonBuffer[key];
     int price_coin = coin["usd"];
     
-    String message = "1 BTC = $";
+    String message = "1"+key+" = $";
     message += price_coin;
 
     Serial.println(message);
